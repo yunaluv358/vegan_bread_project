7a78209831f58bc355c349efdbee62e9275b38db
박우빈 <62981093+yunaluv358@users.noreply.github.com>
2020-07-22 18:37:23 +0900

    18:37

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
+import './home.css'; //Import here your file style
 import { Link } from 'react-router-dom'
-const Home = () => <div className="home">
-    <h1> [홈] </h1>
-    <nav>
-        <Link to={"about"}> [회사소개] </Link>
-        <Link to={"events"}>[이벤트]</Link>
-        <Link to={"products"}>[제품]</Link>
-        <Link to={"contact"}>[고객 지원]</Link>
-    </nav>
-</div>
-export default Home
+
+const Home = () => {
+    return (
+        <div className="nav">
+            <div className="nav-item"> 메뉴
+                <h1> [홈] </h1>
+                <nav>
+                    <Link to={"about"}> [회사소개] </Link>
+                    <Link to={"events"}>[이벤트]</Link>
+                    <Link to={"products"}>[제품]</Link>
+                    <Link to={"contact"}>[고객 지원]</Link>
+                </nav>
+
+
+            </div>
+        </div>)
+}
+
+export default Home;
