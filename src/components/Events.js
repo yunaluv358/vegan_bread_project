91ce88d030cfc0cdb82857b2fc5f3ac8a7fa58c4
yunaluv358 <yunaluv358@gmail.com>
2020-07-26 22:45:46 +0900

    22:40

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {PageTemplate} from './index'
-import {Link} from "react-router-dom";
+// eslint-disable-next-line import {Link} from "react-router-dom";
 const Events = () => <PageTemplate><section className="events">
     <h1> [로그인] </h1>
     </section></PageTemplate>
