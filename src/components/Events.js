7f601d28e82631281b1f426c1298d4fb570fba8f
박우빈 <62981093+yunaluv358@users.noreply.github.com>
2020-07-27 13:53:22 +0900

    13:53

--- src/components/Events.js
+++ src/components/Events.js
@@ -2,6 +2,6 @@ import React from "react";
 import {PageTemplate} from './index'
 import {Link} from "react-router-dom";
 const Events = () => <PageTemplate><section className="events">
-    <h1> [로그인] </h1>
+    <h1> [내게 맞는 빵 찾기] </h1>
     </section></PageTemplate>
 export default Events
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {PageTemplate} from "./index";
 const Products = () => <PageTemplate><section className="products">
-    <h1> [제품] </h1>
+    <h1> [로그인] </h1>
 </section></PageTemplate>
 export default Products
