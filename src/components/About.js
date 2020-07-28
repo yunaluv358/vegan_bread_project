0f9034cfc18c1bde65b80dd68945676f39c5a86d
박우빈 <62981093+yunaluv358@users.noreply.github.com>
2020-07-28 19:32:36 +0900

    19:32

--- src/components/About.js
+++ src/components/About.js
@@ -2,5 +2,8 @@ import React from "react";
 import {PageTemplate} from "./index";
 const About = () => <PageTemplate><section className="events">
     <h1> [빵 목록] </h1>
+
+
+
 </section></PageTemplate>
 export default About
